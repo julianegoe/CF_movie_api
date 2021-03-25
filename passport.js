5adf967abd492ca2cecf533f2cf7199e422a8298
julianegoe <goersch.juliane@dpa.com>
2021-03-25 17:46:01 +0100

    added local and jwt strategies and login routing

---
+++ passport.js
@@ -0,0 +1,54 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const Models = require("./models.js");
+const passportJWT = require("passport-jwt");
+
+let Users = Models.User;
+let JWTStrategy = passportJWT.Strategy;
+let ExtractJwt = passportJWT.ExtractJwt;
+
+passport.use(new LocalStrategy({
+
+    usernameField: 'Username',
+    passwordField: 'Password'
+  },
+  (username, password, done) => 
+  {
+    console.log(username + '  ' + password);
+    Users.findOne({Username: username}, (err, user) =>
+    {
+        if (err)
+        {
+            console.log(err);
+            return done(err)
+        }
+
+        if (!user)
+        {
+            console.log("username not found");
+            return done(null, false, {message: 'Incorrect username or password.'})
+        }
+
+        if (user)
+        {
+            console.log('finished');
+            return done(null, user);
+        }
+    })
+  }));
+
+  passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'secret'
+  },
+  (jwtPayload, done) => 
+  {
+    return Users.findById(jwtPayload._id).then((user) =>
+    {
+        return done(null, user);
+    }).catch((err) =>
+    {
+        console.log(err);
+        return done(err)
+    });
+  }));
