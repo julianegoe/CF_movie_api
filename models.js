7e570d0d6677c502d4ace71d0386613edebebaf1
julianegoe <goersch.juliane@dpa.com>
2021-03-26 17:50:41 +0100

    added Death key to movie schema

--- models.js
+++ models.js
@@ -8,7 +8,8 @@ let movieSchema = mongoose.Schema(
    Director : {
       Name : String,
       Bio : String,
-      Birth : Date
+      Birth : Date,
+      Death : Date,
    },
    Year : Number,
    Genre : {
