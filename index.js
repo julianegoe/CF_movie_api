ecaa19aa5744e6cc6444edee9452892a44672be4
julianegoe <goersch.juliane@dpa.com>
2021-03-26 14:03:17 +0100

    pushed again without changes

--- index.js
+++ index.js
@@ -259,6 +259,6 @@ app.delete("/users/:username", passport.authenticate('jwt', { session: false }),
 
 
 const port = process.env.PORT || 8080;
-app.listen(port, "0.0.0.0",() => {
+app.listen(port, "0.0.0.0", () => {
  console.log("Listening on Port  " + port);
 });
