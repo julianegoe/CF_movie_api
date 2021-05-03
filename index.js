2937f928411299c83819dd9df9d62805fe6cf2e2
julianegoe <goersch.juliane@dpa.com>
2021-05-03 18:16:37 +0200

    allow cors

--- index.js
+++ index.js
@@ -37,18 +37,8 @@ app.use((err, req, res, next) => {
 app.use(express.static('public'));
 let auth = require('./auth')(app);
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 // HTTP requests
 app.get("/", (req, res) => {
